7d9bfdc7dd70b39b8d8f4a0a7487feee2d2d49e2
luis <commentariosweb@gmail.com>
2021-11-23 23:25:03 -0500

    change db

--- db/database.js
+++ db/database.js
@@ -1,8 +1,8 @@
 var Sequelize = require('sequelize');
 
-const database = new Sequelize('kaiohost_neos', 'kaiohost_neo', '@eltc870122', {
+const database = new Sequelize('', '', '', {
     dialect: 'mariadb',
-    host: "scholasonline.ml",
+    host: "localhost",
     dialectOptions: {
       // Your mariadb options here
       // connectTimeout: 1000
